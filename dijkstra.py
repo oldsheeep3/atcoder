a20cc80f2e9bf896da64beadd5bdcd9423c4a2e7
oldsheeep3 <js2oyb@gmail.com>
2024-07-27 20:50:26 +0900

    abc

---
+++ dijkstra.py
@@ -0,0 +1,19 @@
+from heapq import heappush, heappop
+def dijkstra(G, s, inf):
+    dist = [inf] * len(G)
+    dist[s] = 0
+    pq = [(0,s)]
+    while pq:
+        d, v = heappop(pq)
+        if d > dist[v]:
+            continue
+        for u, weight in G[v]:
+            nd = d + weight
+            if dist[u] > nd:
+                dist[u] = nd
+                heappush(pq, (nd, u))
+    return dist
+
+# N:要素数
+# G = [[] for _ in range(N)]
+# G[始点] -> (終点,重み)
